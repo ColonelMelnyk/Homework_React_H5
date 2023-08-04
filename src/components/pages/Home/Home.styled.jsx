e1075aa94272e4b313866d28cc6592b279bdac52
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-08-04 22:13:27 +0300

    pt2

---
+++ src/components/pages/Home/Home.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+export const TrendingList = styled.div`
+list-style-type:none;
+`
+export const TrendingTitle = styled.div`
+
+font-size: 32px;
+`
+export const TrendingMember = styled.div`
+margin-top: 10px;
+`
